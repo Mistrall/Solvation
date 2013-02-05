99eb2dc392967e9654a2794aeb37c8c7e57f237d
Tony G <mipt.shootnick.spam@gmail.com>
2013-02-06 01:21:34 +0400

    Fix preceding jobs binding

--- Solvation/DataObjects/BasicStructures/Job.cs
+++ Solvation/DataObjects/BasicStructures/Job.cs
@@ -1,13 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace DataObjects.BasicStructures
 {
 	public class Job
 	{
+		private IEnumerable<int> precedingJobs; 
+
 		public int Number { get; set; }
-		public IEnumerable<int> PrecedingJobs { get; set; }
-		public string PrecedingJobsStr { get; set; }
+		public IEnumerable<int> PrecedingJobs 
+		{ 
+			get { return precedingJobs; }
+			set { precedingJobs = value; }
+		}
+		public string PrecedingJobsStr 
+		{ 
+			get
+			{
+				return String.Join(",", precedingJobs.Select(j => j.ToString(CultureInfo.InvariantCulture)));
+			}
+			set
+			{
+				if (value != null)
+				{
+					PrecedingJobs = value.Split(',').Select(Int32.Parse).ToList();	
+				}
+			} 
+		}
 		public double FullWorkVolume { get; set; }
 		public double MinimumIntencity { get; set; }
 		public double MaximumIntencity { get; set; }
@@ -18,13 +39,8 @@ namespace DataObjects.BasicStructures
 		public Job(int number, string precedingJobs, double fullWorkVolume, double minimumIntencity, double maximumIntencity)
 		{
 			Number = number;
+			PrecedingJobs=new List<int>();
 			PrecedingJobsStr = precedingJobs;
-			if (!string.IsNullOrEmpty(precedingJobs))
-			{
-				var pj = Enumerable.Cast<int>(precedingJobs.Split(','));
-				PrecedingJobs = pj;
-				
-			}
 			FullWorkVolume = fullWorkVolume;
 			MinimumIntencity = minimumIntencity;
 			MaximumIntencity = maximumIntencity;
